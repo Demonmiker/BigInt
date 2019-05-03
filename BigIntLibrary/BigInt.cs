d34ca1c8c73599efffe7c6f6232079360e042add
Demonmiker <Demonmiker@ya.ru>
2019-05-03 18:55:58 +0300

    Основа проекта

---
+++ BigIntLibrary/BigInt.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigIntLibrary
+{
+    public class BigInt 
+    {
+        #region Структура
+        #endregion
+
+        #region Добавление
+
+        #endregion
+
+        #region Вычитание
+
+        #endregion
+
+        #region Умножение
+
+        #endregion
+
+        #region Деление
+
+        #endregion
+
+        #region Перевод в другие типы
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public void Set(int num)
+        {
+
+        }
+
+        public void Set(long num)
+        {
+
+        }
+
+        public void Set(ulong num)
+        {
+
+        }
+
+        public void Set(uint num)
+        {
+
+        }
+        #endregion
+    }
+}
